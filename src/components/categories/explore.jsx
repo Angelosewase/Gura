da9470dd3c328c60478818c2fe1dd5cb1788dee1
Angel <sewasejo8@gmail.com>
2024-04-01 16:24:28 +0200

    add products to the explore category

--- src/components/categories/explore.jsx
+++ src/components/categories/explore.jsx
@@ -1,8 +1,42 @@
-import React from "react";
-import rightarrow from "/images/arrow-small-right.svg";
-import leftarrow from "/images/arrow-small-left.svg";
+import React, { forwardRef, useImperativeHandle } from "react";
+import {products} from '../../products.js'
+import Product from '../product'
+
+const Ourproduts=forwardRef((props,ref)=>{
+  const [more ,setMore] = React.useState(false)
+  const Products =[]
+  for (let index = 10; index < 20; index++) {
+    const element = products[index];
+    Products.push(<Product {...element}/>)
+  }
+  if(more){
+  for (let index = 30; index < 40; index++) {
+    const element = products[index];
+    Products.push(<Product {...element}/>)
+  }
+
+}
+function  handleCLick(){
+  setMore(prev =>!prev)
+}
+
+useImperativeHandle(ref,()=>({
+  handleCLick
+}))
+  return(<>
+   <div className="flex justify-evenly mr-28 flex-wrap">
+    {Products}
+   </div>
+  </>)
+})
 
 function Explore(){
+  const productsref = React.useRef(null)
+  const [buttonclicked,setButtonclicked] = React.useState(false)
+  function handleclicl(){
+    productsref.current.handleCLick()
+    setButtonclicked( prev => !prev)
+  }
     return (
         <>
           <div className="flex flex-row  w-11/12 justify-between ">
@@ -15,13 +49,15 @@ function Explore(){
             </div>
             <div className="flex flex-row items-center">
               <button className="w-8 h-8 rounded-full bg-gray-100 mx-1">
-                <img src={leftarrow} alt="the left arrow" className="w-8" />
+                <img src={"/images/arrow-small-left.svg"} alt="the left arrow" className="w-8" />
               </button>
               <button className="w-8 h-8 rounded-full  bg-gray-100">
-                <img src={rightarrow} alt="the right arrow" className="w-8" />
+                <img src={"/images/arrow-small-right.svg"} alt="the right arrow" className="w-8" />
               </button>
             </div>
           </div>
+          <Ourproduts ref={productsref}/>
+          <button className="py-2 px-8 bg-red-500 rounded  text-white ml-[45%] mt-4" onClick={handleclicl}>{buttonclicked ? "show less" : "view all products"}</button>
         </>
     )
 }
